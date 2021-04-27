e4d216eca177f40a4c5ddac35777f73977e23e10
lucas66688 <83282846+lucas66688@users.noreply.github.com>
2021-04-27 10:33:22 -0300

    Add files via upload

---
+++ zeusmenu.js
@@ -0,0 +1,12 @@
+const nagatomenu = (prefix, pushname) => {
+    return `❍ *Comandos do nagato*
+    
+    ║➩ ❍ ${prefix}setprefix
+    ║➩ ❍ ${prefix}block
+    ║➩ ❍ ${prefix}tm
+    ║➩ ❍ ${prefix}tmctt
+    ║➩ ❍ ${prefix}clearall`
+
+}
+
+exports.nagatomenu = nagatomenu
